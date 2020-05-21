0cde3d522b17ed21a5f2bdc2e608fa55af99a909
Jordan Dickson <jfdickson25@gmail.com>
2020-05-20 23:27:31 -0600

    team activity completed

--- week5/ta05/main.js
+++ week5/ta05/main.js
@@ -1,6 +1,35 @@
 import Hikes from './controller.js';
-//on load grab the array and insert it into the page
 const controller = new Hikes('hikes');
-window.addEventListener('load', () => {
-  controller.showHikeList();
-});
+
+
+ window.addEventListener('load', () => {
+   controller.showHikeList();
+   delay();
+ });
+
+ function displayAll() {
+  window.location.reload();
+ }
+
+ function display0() {
+  controller.showHike('Bechler Falls');
+  document.getElementById("back").addEventListener('click', displayAll);
+ }
+
+ function display1() {
+  controller.showHike('Teton Canyon');
+  document.getElementById("back").addEventListener('click', displayAll);
+}
+
+ function display2() {
+  controller.showHike('Denanda Falls');
+  document.getElementById("back").addEventListener('click', displayAll);
+ }
+
+//Add event listener for one hike
+function delay() {
+  document.getElementById("0").addEventListener('click', display0);
+  document.getElementById("1").addEventListener('click', display1);
+  document.getElementById("2").addEventListener('click', display2);
+
+}
--- week5/ta05/model.js
+++ week5/ta05/model.js
@@ -1,6 +1,6 @@
-// normally the model would have more going on...retrieving the hikes from a database, filtering, etc. Our model will be very simple.  We could simply export the hikeList, but a better pattern would be to create a 'getter' function to do it instead. That way as our model changed...we could simply change the getter function and anything using it should be able to remain the same.
 const hikeList = [
     {
+      id: '0',
       name: 'Bechler Falls',
       imgSrc: 'https://images-na.ssl-images-amazon.com/images/I/81WzhN3F2PL._AC_SX522_.jpg',
       imgAlt: 'Image of Bechler Falls',
@@ -12,6 +12,7 @@ const hikeList = [
         'Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road.Drive to the end of the Cave Falls road. There is a parking area at the trailhead.'
     },
     {
+      id: '1',
       name: 'Teton Canyon',
       imgSrc: 'https://cdn.jacksonholewy.net/images/content/14443_1131_Teton_Canyon_Alaska_Basin_Trail_Wyoming_lg.jpg',
       imgAlt: 'Image of Teton Canyon',
@@ -23,6 +24,7 @@ const hikeList = [
         'Take Highway 33 East to Driggs. Turn left onto Teton Canyon Road. Follow that road for a few miles then turn right onto Staline Raod for a short distance, then left onto Alta Road. Veer right after Alta back onto Teton Canyon Road. There is a parking area at the trailhead.'
     },
     {
+      id: '2',
       name: 'Denanda Falls',
       imgSrc: 'https://i.pinimg.com/originals/d3/17/10/d317102481a3b96cdb21cd1ec2aeaa8f.jpg',
       imgAlt: 'Image of Denanda Falls',
@@ -40,6 +42,7 @@ const hikeList = [
     }
   
     getHikeByName(name) {
+      // From teacher solution
       return hikeList.find(hike => hike.name === name);
     }
   }
